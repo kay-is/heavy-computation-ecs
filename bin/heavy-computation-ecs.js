8e28e356960dad82d80bec5b8ef5e360d9705bd9
Kay Plößer <k@kay.is>
2020-12-31 14:54:25 +0000

    Connect services

--- bin/heavy-computation-ecs.js
+++ bin/heavy-computation-ecs.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
-const cdk = require('@aws-cdk/core');
-const { HeavyComputationEcsStack } = require('../lib/heavy-computation-ecs-stack');
+const cdk = require("@aws-cdk/core");
+const {
+  HeavyComputationEcsStack,
+} = require("../lib/heavy-computation-ecs-stack");
 
 const app = new cdk.App();
-new HeavyComputationEcsStack(app, 'HeavyComputationEcsStack');
+new HeavyComputationEcsStack(app, "HeavyComputationEcsStack");
